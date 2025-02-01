95da4391c6b5441293f4174e800bba262f186301
ali-kk <noori.noori101@gmail.com>
2025-02-01 09:25:31 +0300

    add

--- myuni/urls.py
+++ myuni/urls.py
@@ -19,6 +19,6 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-  
+  path('',include('home.urls')),
    path('reg/', include('register.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
